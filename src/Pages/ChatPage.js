bddc4116e2266b803f75637aa39b228575875e42
nihal <nihalahmed9672@gmail.com>
2023-08-03 17:59:27 +0530

    chat page

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -12,6 +12,9 @@ import account from "../Photos/account.jpg";
 import account2 from "../Photos/account2.jpg";
 import SideMenu from "../Components/SideMenu";
 import Page from "../Layouts/Pages";
+import sendIcon from '../Photos/sendIcon.png'
+import micIcon from '../Photos/micIcon.png'
+import smileIcon from '../Photos/smileIcon.png'
 
 const ChatPage = () => {
   const [chats, setChats] = useState(true);
@@ -35,91 +38,193 @@ const ChatPage = () => {
     setText("");
   };
 
+  const Groups = [
+    {
+      id: 1,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      groupName: 'Group1'
+    },
+    {
+      id: 2,
+      image: 'account2.jpg',
+      name: 'Sierra',
+      groupName: 'Group1'
+    },
+    {
+      id: 3,
+      image: 'account2.jpg',
+      name: 'Naveen',
+      groupName: 'Group1'
+    },
+    {
+      id: 4,
+      image: 'account2.jpg',
+      name: 'Naveen',
+      groupName: 'Group1'
+    },
+    {
+      id: 5,
+      image: 'account2.jpg',
+      name: 'Naveen',
+      groupName: 'Group1'
+    },
+    {
+      id: 6,
+      image: 'account2.jpg',
+      name: 'Naveen',
+      groupName: 'Group1'
+    },
+
+  ]
+
+  const AllChats = [
+    {
+      id: 1,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      time: '12:00 AM',
+      message:'Hey! How are you'
+    },
+    {
+      id: 2,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      time: '12:00 AM',
+      message:'Hey! How are you'
+    },
+    {
+      id: 3,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      time: '12:00 AM',
+      message:'Hey! How are you'
+    },
+    {
+      id: 4,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      time: '12:00 AM',
+      message:'Hey! How are you'
+    },
+    {
+      id: 5,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      time: '12:00 AM',
+      message:'Hey! How are you'
+    },
+    {
+      id: 6,
+      image: 'account2.jpg',
+      name: 'Nihal',
+      time: '12:00 AM',
+      message:'Hey! How are you'
+    },
+  ]
+
   return (
     <Page
       pageContent={
         <>
           {/* background */}
-          <div className="bg-[#FEF8FD] h-full">
+          <div className="bg-[#FEF8FD] h-[100%]">
             {/* chat space */}
-            <div className=" h-full flex pt-[2%] justify-center pl-[5%] ">
-              <div className="h-[85%] w-[85%] bg-white rounded-3xl overflow-hidden shadow-2xl">
-                <div className="flex">
+            <div className=" h-full flex py-10 px-24 justify-center  ">
+              <div className="  bg-white h-full rounded-3xl  shadow-2xl">
+                <div className="flex h-full w-full">
                   {/* left bar */}
-                  <div className="pt-4 px-4 h-[80vh] w-[25%] border-r-2">
-                    <div className="flex items-center gap-2 mt-3 p-2 shadow-2xl bg-white rounded-3xl">
+                  <div className="pt-4 px-4   w-[25%] border-r-2">
+                    <div className="flex  items-center gap-2 mt-3 p-2 shadow-2xl bg-white rounded-3xl">
                       <FiSearch color="black" />
-                      <input placeholder="Search" className="outline-none" />
+                      <input placeholder="Search" className="outline-none overflow-hidden" />
                     </div>
 
                     <h2 className="mt-5 font-semibold">Group's</h2>
 
                     {/* persons */}
-                    <div className="flex gap-4 pt-2">
-                      <div className=" w-[20%] flex flex-col items-center">
-                        <img src={account} className="rounded-full" />
-                        <p className="font-semibold">Sierra</p>
-                        <p className="text-xs">Group</p>
-                      </div>
-                      <div className=" w-[20%] flex flex-col items-center">
-                        <img src={account} className="rounded-full" />
-                        <p className="font-semibold">Sierra</p>
-                        <p className="text-xs">Group</p>
-                      </div>
-                      <div className=" w-[20%] flex flex-col items-center">
-                        <img src={account} className="rounded-full" />
-                        <p className="font-semibold">Sierra</p>
-                        <p className="text-xs">Group</p>
-                      </div>
-                      <div className=" w-[20%] flex flex-col items-center">
-                        <img src={account} className="rounded-full" />
-                        <p className="font-semibold">Sierra</p>
-                        <p className="text-xs">Group</p>
+                    {/* <div className="flex  overflow-x-scroll pt-2">
+                      {Groups.map((item) => (
+                        <div key={item.id} className=" mx-6 flex flex-col items-center">
+                          <img src={item.image} className="rounded-full" />
+                          <p className="font-semibold">{item.name}</p>
+                          <p className="text-xs">{item.groupName}</p>
+                        </div>
+                      ))}
+
+                      
+                    </div> */}
+                    <div className="flex overflow-x-scroll py-4" style={{ width: "100%" }}>
+                      <div className="flex" style={{ width: "fit-content" }}>
+                        {Groups.map((item) => (
+                          <div key={item.id} className="w-24  flex flex-col items-center">
+                          <div className="w-14 h-14 border-red-600 border-2 rounded-full flex items-center justify-center">
+                          <img
+                              src={item.image}
+                              alt={item.name} // Added alt attribute for accessibility
+                              className="w-12 h-12 rounded-full object-cover" // Adjust size of the image
+                            />
+                          </div>
+                            
+                            <p className="font-semibold text-[14px]">{item.name}</p>
+                            <p className=" text-[12px]">{item.groupName}</p>
+                          </div>
+                        ))}
                       </div>
                     </div>
 
                     {/* chats */}
-                    <div className=" flex pt-3 justify-between px-5 relative">
+                    <div className=" flex pt-3 justify-between  relative">
                       <div
                         className={
                           chats
-                            ? "font-bold text-[#8B1539] cursor-pointer"
-                            : "font-bold text-[#C4C4C4] cursor-pointer"
+                            ? "font-bold border-b-2 w-[50%] flex items-center justify-center transform-all ease-in-out duration-500 border-[#8B1539] text-[#8B1539] cursor-pointer"
+                            : "font-bold border-b-2 w-[50%] flex items-center justify-center border-[#C4C4C4] text-[#C4C4C4] cursor-pointer"
                         }
                         onClick={toggleReq}
                       >
                         <h2 className="">Chat</h2>
-                        <p className="absolute top-[50%]">_______________</p>
+                       
                       </div>
                       <div
                         className={
                           !chats
-                            ? "font-bold text-[#8B1539] cursor-pointer"
-                            : "font-bold text-[#C4C4C4] cursor-pointer"
+                            ? "font-bold border-b-2 w-[50%] flex items-center justify-center transform-all ease-in-out duration-500 border-[#8B1539] text-[#8B1539] cursor-pointer"
+                            : "font-bold border-b-2 w-[50%] flex items-center justify-center border-[#C4C4C4] text-[#C4C4C4] cursor-pointer"
                         }
                         onClick={toggleReq}
                       >
-                        <h2>Requests</h2>
-                        <p className="absolute top-[50%] right-[7%]">
-                          _______________
-                        </p>
+                        <h2 className="">Requests</h2>
+                        
                       </div>
                     </div>
 
                     {chats && (
-                      <div className="flex pt-4 items-center">
+                      <div className="flex flex-col mt-2 gap-6 h-[50%] w-full overflow-y-scroll " style={{  }}>
+                      
+                      {AllChats.map((item)=> (
+                        <div className="flex flex-row gap-6" key={item.id}>
+                        <div className="relative">
                         <img
-                          src={account2}
-                          className="h-[20%] w-[20%] rounded-full"
+                          src={item.image} alt={item.name}
+                          className="h-12 w-12 rounded-full  "
                         />
-                        <div>
-                          <div className="flex justify-between items-center w-[115%] px-1">
-                            <h1 className="font-bold">Iqra</h1>
-                            <p className="text-xs">Yesterday</p>
+                        <div className=" absolute top-0 right-0 h-3 w-3 flex items-center justify-center rounded-full text-[10px] text-white font-poppins bg-[#C31A7F]">2</div>
+                        <div className=" absolute bottom-0 right-2 h-2 w-2 flex items-center justify-center rounded-full text-[10px] text-white font-poppins bg-[#11D800]"></div>
+                        </div>
+                       
+                         <div>
+                          <div className="flex flex-row  items-center gap-24  ">
+                            <h1 className="font-bold">{item.name}</h1>
+                            <p className="text-[12px] ">{item.time}</p>
                           </div>
 
-                          <p className="px-1 text-xs">Hey! , how are you</p>
+                          <p className=" text-[12px]  font-poppins">{item.message}</p>
                         </div>
+                        </div>
+                      ))}
+                        
+                      
                       </div>
                     )}
 
@@ -155,23 +260,24 @@ const ChatPage = () => {
                   </div>
 
                   {/* right side */}
-                  <div className="w-full flex flex-col ">
-                    <div className="flex h-[12vh]">
-                      <div className=" w-full flex px-6 items-center  ">
-                        <img src={account} className="rounded-full w-[6%]" />
-                        <h2 className="px-2 font-bold">Sierra's Group</h2>
-                      </div>
+                  <div className=" w-full    ">
 
-                      <div className="flex gap-4 items-center mr-6">
-                        <AiOutlineVideoCamera />
-                        <BsTelephone />
-                        <BsThreeDotsVertical />
-                      </div>
-                    </div>
 
                     {/* main chat */}
-                    <div className="h-full bg-[#FFF8FC] relative flex flex-col justify-end">
-                      <div className="flex flex-row absolute  w-full mb-11">
+                    <div className=" bg-[#F5F5F5] h-full overflow-hidden rounded-r-3xl  flex flex-col justify-between  ">
+                      <div className="flex bg-white h-[12vh]">
+                        <div className=" w-full flex px-6 items-center  ">
+                          <img src={account} className="rounded-full w-[6%]" />
+                          <h2 className="px-2 font-bold">Sierra's Group</h2>
+                        </div>
+
+                        <div className="flex gap-4 items-center mr-6">
+                          <AiOutlineVideoCamera />
+                          <BsTelephone />
+                          <BsThreeDotsVertical />
+                        </div>
+                      </div>
+                      <div className="flex flex-row   w-full mb-7">
                         <div className="bg-white w-full p-2 px-4 shadow-2xl rounded-2xl flex justify-between items-center mx-5">
                           <input
                             placeholder="Type your message...."
@@ -188,11 +294,11 @@ const ChatPage = () => {
                             className="bg-[#EFC319] p-2 rounded-full shadow-gray-950 shadow-2xl"
                           >
                             {" "}
-                            <AiOutlineSend size={25} color="white" />
+                            <AiOutlineSend size={20} color="white" />
                           </div>
                           <div className="bg-[#ffffff] p-2 rounded-full shadow-gray-950 shadow-2xl">
                             {" "}
-                            <GrMicrophone size={25} />
+                            <GrMicrophone size={20} />
                           </div>
                         </div>
                       </div>
